e4539c8bf64829dc2c0c9e114b3898b26af7cbf5
wokerNM <nicholasmilligan2019@outlook.com>
2025-12-05 17:00:44 -0800

    Initial Commit

---
+++ lua/indent-num/init.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+function M.Indentnum(count)
+	count = tonumber(count) or 1
+	count = math.max(count, 1)
+
+	for _ = 1, count do
+		vim.cmd("normal! >>")
+	end
+end
+
+function M.Unindentnum(count)
+	count = tonumber(count) or 1
+	count = math.max(count, 1)
+
+	for _ = 1, count do
+		vim.cmd("normal! <<")
+	end
+end
+
+function M.setup()
+	vim.api.nvim_create_user_command("Indentnum", function(opts)
+		local count = tonumber(opts.args) or 1
+		count = math.max(count, 1)
+		for _ = 1, count do
+			vim.cmd("normal! >>")
+		end
+	end, {
+		nargs = "?",
+	})
+
+	vim.api.nvim_create_user_command("Unindentnum", function(opts)
+		local count = tonumber(opts.args) or 1
+		count = math.max(count, 1)
+		for _ = 1, count do
+			vim.cmd("normal! <<")
+		end
+	end, {
+		nargs = "?",
+	})
+
+	vim.api.nvim_set_keymap(
+		"n",
+		"<Tab>",
+		":lua require('indent-num').Indentnum(vim.v.count)<CR>",
+		{ noremap = true, silent = true }
+	)
+
+	vim.api.nvim_set_keymap(
+		"n",
+		"<S-Tab>",
+		":lua require('indent-num').Unindentnum(vim.v.count)<CR>",
+		{ noremap = true, silent = true }
+	)
+end
+
+return M
